63dd3d0fe4f0a7b6a948030d568730c5bc3f8dc3
Artem Bohuslavsky <newlifeboguslavskiy@gmail.com>
2025-06-06 16:07:39 +0300

    lesson-1-5

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -1,8 +1,23 @@
+<script setup>
+const course = useCourse();
+const route = useRoute();
+
+const chapter = computed(() => {
+  return course.chapters.find(chapter => chapter.slug === route.params.chapterSlug);
+})
+
+const lesson = computed(() => {
+  return chapter.value.lessons.find(lesson => lesson.slug === route.params.lessonSlug);
+})
+
+console.log('Course:', course);
+</script>
+
 <template>
   <div class="prose">
     <h2>Lesson</h2>
     <p>This is a lesson</p>
-    <p>Chapter slug: {{ $route.params.chapterSlug }}</p>
-    <p>Lesson slug: {{ $route.params.lessonSlug }}</p>
+    <p>Chapter slug: {{ chapter.title }}</p>
+    <p>Lesson slug: {{ lesson.title }}</p>
   </div>
 </template>
