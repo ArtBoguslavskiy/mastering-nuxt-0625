c1fa4b060c7c1025211327da5057efc9da034dd7
Artem Bohuslavsky <newlifeboguslavskiy@gmail.com>
2025-06-06 17:46:15 +0300

    lesson-1-12

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -1,4 +1,4 @@
-<script setup>
+lear<script setup>
 const course = useCourse();
 const route = useRoute();
 
@@ -17,6 +17,30 @@ const title = computed(() => {
 useHead({
   title,
 })
+
+const progress = useState('progress', () => {
+  return [];
+})
+
+const isLessonComplete = computed(() => {
+  if(!progress.value[chapter.value.number - 1]) {
+    return false;
+  }
+
+  if(!progress.value[chapter.value.number - 1][lesson.value.number - 1]) {
+    return false;
+  }
+
+  return progress.value[chapter.value.number - 1][lesson.value.number - 1];
+})
+
+const toggleComplete = () => {
+  if(!progress.value[chapter.value.number - 1]) {
+    progress.value[chapter.value.number - 1] = [];
+  }
+
+  progress.value[chapter.value.number - 1][lesson.value.number - 1] = !isLessonComplete.value;
+}
 </script>
 
 <template>
@@ -46,5 +70,9 @@ useHead({
       :video-id="lesson.videoId"
     />
     <p>{{ lesson.text }}</p>
+    <LessonCompleteButton
+      :model-value="isLessonComplete"
+      @update:model-value="toggleComplete"
+    />
   </div>
 </template>
