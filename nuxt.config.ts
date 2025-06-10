339fddc623c7f43f79d95a842cb47e2400c35135
Artem Bohuslavsky <newlifeboguslavskiy@gmail.com>
2025-06-10 14:30:47 +0300

    lesson-4-4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', '@vueuse/nuxt']
+  modules: [
+    '@nuxt/eslint',
+    '@nuxtjs/tailwindcss',
+    '@vueuse/nuxt',
+    '@nuxtjs/supabase'
+  ]
 })
