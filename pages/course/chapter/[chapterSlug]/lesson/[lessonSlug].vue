3d565114e809d9a9a3cb827c7d1e30980559642a
Artem Bohuslavsky <newlifeboguslavskiy@gmail.com>
2025-06-10 13:08:50 +0300

    lesson-3-5

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -2,29 +2,39 @@
 const course = useCourse();
 const route = useRoute();
 
+definePageMeta({
+  validate({ params }) {
+    const course = useCourse();
+
+    const chapter = course.chapters.find((chapter) => chapter.slug === params.chapterSlug);
+
+    if(!chapter) {
+      return createError({
+        statusCode: 404,
+        statusMessage: 'Chapter not found',
+      })
+    }
+
+    const lesson = chapter.lessons.find((lesson) => lesson.slug === params.lessonSlug);
+
+    if(!lesson) {
+      return createError({
+        statusCode: 404,
+        statusMessage: 'Lesson not found',
+      })
+    }
+
+    return true;
+  }
+})
+
 const chapter = computed(() => {
   return course.chapters.find(chapter => chapter.slug === route.params.chapterSlug);
 });
 
-if(!chapter.value) {
-  throw createError({
-    statusCode: 404,
-    statusMessage: 'Chapter not found',
-    message: `Chapter with slug "${route.params.chapterSlug}" not found.`
-  });
-}
-
 const lesson = computed(() => {
   return chapter.value.lessons.find(lesson => lesson.slug === route.params.lessonSlug);
 });
-
-if(!lesson.value) {
-  throw createError({
-    statusCode: 404,
-    statusMessage: 'Lesson not found',
-    message: `Lesson with slug "${route.params.lessonSlug}" not found.`
-  });
-}
 
 const title = computed(() => {
   return `Lesson ${lesson.value.title} - ${course.title}`;
