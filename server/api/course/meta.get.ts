e2174d115b7c5332665532fed8ee4a78365805dc
Artem Bohuslavsky <newlifeboguslavskiy@gmail.com>
2025-06-13 13:24:48 +0300

    lesson-5-4

---
+++ server/api/course/meta.get.ts
@@ -0,0 +1,53 @@
+import type { Course, Chapter } from "~/types/course";
+import course from '~/server/courseData';
+
+const typedCourse: Course = course;
+
+type OutlineBase = {
+  title: string;
+  slug: string;
+  number: number;
+};
+
+type OutlineChapter = OutlineBase & {
+  lessons: OutlineLesson[];
+};
+
+type OutlineLesson = OutlineBase & {
+  path: string;
+};
+
+type CourseMeta = {
+  title: string;
+  chapters: OutlineChapter[];
+};
+
+export default defineEventHandler((): CourseMeta => {
+  const outline: OutlineChapter[] = typedCourse.chapters.reduce(
+    (prev: OutlineChapter[], next: Chapter) => {
+      const lessons: OutlineLesson[] = next.lessons.map(
+        (lesson) => ({
+          title: lesson.title,
+          slug: lesson.slug,
+          number: lesson.number,
+          path: `/course/chapter/${next.slug}/lesson/${lesson.slug}`,
+        })
+      );
+
+      const chapter: OutlineChapter = {
+        title: next.title,
+        slug: next.slug,
+        number: next.number,
+        lessons,
+      };
+
+      return [...prev, chapter];
+    },
+    []
+  );
+
+  return {
+    title: typedCourse.title,
+    chapters: outline,
+  };
+});
