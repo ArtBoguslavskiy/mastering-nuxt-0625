1c80f1acbaf68111094b99740c787a6707043351
Artem Bohuslavsky <newlifeboguslavskiy@gmail.com>
2025-06-13 14:57:04 +0300

    lesson-5-6

--- composables/useLesson.ts
+++ composables/useLesson.ts
@@ -1,12 +1,25 @@
-export default async (chapterSlug: string, lessonSlug: string) => {
-  const { data, error } = await useFetch(`/api/course/chapter/${chapterSlug}/lesson/${lessonSlug}`);
+import { StorageSerializers } from '@vueuse/core';
+import type { LessonWithPath } from "~/types/course";
 
-  if (error.value) {
-    throw createError({
-      ...error.value,
-      statusMessage: `Could not fetch lesson "${lessonSlug}" in chapter "${chapterSlug}"`,
-    })
+export default async (chapterSlug: string, lessonSlug: string) => {
+  const url = `/api/course/chapter/${chapterSlug}/lesson/${lessonSlug}`;
+  const lesson = useSessionStorage<LessonWithPath>(url, null, {
+    serializer: StorageSerializers.object,
+  });
+
+  if (!lesson.value) {
+    const { data, error } = await useFetch<LessonWithPath>(url);
+
+    if (error.value) {
+      throw createError({
+        ...error.value,
+        statusMessage: `Could not fetch lesson "${lessonSlug}" in chapter "${chapterSlug}"`,
+      })
+    }
+    lesson.value = data.value;
+  } else {
+    console.log(`Getting lesson ${lessonSlug} in chapter ${chapterSlug} from cache`);
   }
 
-  return data;
+  return lesson;
 }
